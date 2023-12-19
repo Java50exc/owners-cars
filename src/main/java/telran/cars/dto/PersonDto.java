d3a235cda0efb77de9fe9fc07538ea9352817678
Yuragranovsky <yuragranovsky@gmail.com>
2023-12-19 19:41:20 +0200

    CW #55 Validation constraints and constants

--- src/main/java/telran/cars/dto/PersonDto.java
+++ src/main/java/telran/cars/dto/PersonDto.java
@@ -1,5 +1,11 @@
 package telran.cars.dto;
-
-public record PersonDto(long id, String name, String birthDate, String email) {
+import jakarta.validation.constraints.*;
+import static telran.cars.api.ValidationConstants.*;
+public record PersonDto(@NotNull(message=MISSING_PERSON_ID_MESSAGE)
+@Min(value=MIN_PERSON_ID_VALUE, message=WRONG_MIN_PERSON_ID_VALUE) @Max(value=MAX_PERSON_ID_VALUE,
+message=WRONG_MAX_PERSON_ID_VALUE )
+Long id, @NotEmpty (message=MISSING_PERSON_NAME_MESSAGE) String name,
+@NotEmpty(message=MISSING_BIRTH_DATE_MESSAGE) @Pattern(regexp=BIRTH_DATE_REGEXP, message=WRONG_DATE_FORMAT)
+String birthDate, @NotEmpty(message=MISSING_PERSON_EMAIL) @Email(message=WRONG_EMAIL_FORMAT) String email) {
 	
 }
